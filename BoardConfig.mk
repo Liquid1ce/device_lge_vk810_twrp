1c2ad892ddb3c08efbe151bfba0ec9e64723f982
Liquid1ce <pokebladez420@gmail.com>
2016-10-23 18:47:25 -0400

    d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,12 +15,6 @@ TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_NO_BOOTLOADER := true
 
-# Architecture
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := krait
 
 # Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
